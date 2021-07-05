d7a2216fb2dcbb75362fc3882cec8de8f660bbc3
Mubashir <mubashirhasan716@gmail.com>
2021-07-05 22:34:24 +0530

    done

--- pencil.py
+++ pencil.py
@@ -2,16 +2,21 @@ import cv2
 
 image = cv2.imread("trump.jpeg")
 
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("President Trump", gray_image)
+def sketch():
 
-inverted_image = 255 - gray_image
-blurred = cv2.GaussianBlur(inverted_image, ksize=(21, 21),sigmaX=0, sigmaY=0)
+  gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  cv2.imshow("President Trump", gray_image)
 
-inverted_blurred = 255 - blurred
-pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=256.0)
-cv2.imshow("Sketch", pencil_sketch)
+  inverted_image = 255 - gray_image
+  blurred = cv2.GaussianBlur(inverted_image, ksize=(21, 21),sigmaX=0, sigmaY=0)
 
-cv2.imshow("pencil sketch", pencil_sketch)
-cv2.imwrite("trump.png",pencil_sketch)
-cv2.waitKey(0)
+  inverted_blurred = 255 - blurred
+  pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=256.0)
+  cv2.imshow("Sketch", pencil_sketch)
+
+  cv2.imshow("pencil sketch", pencil_sketch)
+  cv2.imwrite("trump.png",pencil_sketch)
+  cv2.waitKey(0)
+
+sketch(image)
+
